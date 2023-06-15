1c4c70b3d6d83457603336f70824a272c3551bf3
rfkma <rfkmhmdmr@gmail.com>
2023-06-15 16:31:28 +0200

    version_2_(grafica)

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,6 +1,6 @@
 
 String baseurl = "https://api.openai.com/v1/chat/completions";
 
-String apikey = "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";
+String apikey = "sk-XXX";
 
 String model = "gpt-3.5-turbo-0301";
