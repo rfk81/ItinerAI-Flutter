43eb16d374373bddcb83fde69117c59beceac67b
rfkma <rfkmhmdmr@gmail.com>
2023-06-15 17:35:58 +0200

    Version_2(Gráfica)

--- lib/constants/api_consts.dart
+++
@@ -1,6 +0,0 @@
-
-String baseurl = "https://api.openai.com/v1/chat/completions";
-
-String apikey = "sk-XXX";
-
-String model = "gpt-3.5-turbo-0301";
--- lib/constants/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color scaffoldBackgroundColor = Color.fromARGB(255, 250, 250, 250);
-Color cardColor = Color.fromARGB(255, 48, 136, 214);
